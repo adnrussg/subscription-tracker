75050ac57ba6e05abeb23cfd98f220ff5c1b7fe3
adnrussg <russelgarcia1551@gmail.com>
2025-03-03 21:07:40 +0800

    Workflow subscription reminder using Upstash

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
 import Subscription from "../models/subscription.model.js";
+import workflowClient from "../config/upstash.js";
+import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -7,7 +9,18 @@ export const createSubscription = async (req, res, next) => {
       user: req.user._id,
     });
 
-    res.status(201).json({ success: true, data: subscription });
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription._id,
+      },
+      headers: {
+        "content-type": "application/json",
+      },
+      retries: 0,
+    });
+
+    res.status(201).json({ success: true, data: { subscription, workflowRunId} });
   } catch (error) {
     next(error);
   }
@@ -15,7 +28,7 @@ export const createSubscription = async (req, res, next) => {
 
 export const getUserSubscriptions = async (req, res, next) => {
   try {
-    if(req.user.id !== req.params.id) {
+    if (req.user.id !== req.params.id) {
       const error = new Error("You are not the owner of this account");
       error.statusCode = 401;
       throw error;
